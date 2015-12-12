49275c46166c91df35b1e49299a40b267676c8ad
Luke Szanto <luke@cardcorp.net.au>
2015-12-12 12:24:32 +1100

    Add logging and look into config

--- hyde.php
+++ hyde.php
@@ -1,3 +1,4 @@
+#!/usr/bin php
 <?php
 
 // composer
@@ -6,25 +7,48 @@ require __DIR__ . '/vendor/autoload.php';
 // grab yaml component
 use Symfony\Component\Yaml\Yaml;
 
-// grab site pages
-$site_pages = glob(__DIR__ . '/site/*.html');
+// logger fns
+function info($str) {
+    echo "\033[0;32mHyde >> {$str}\n\033[0m";
+}
+function err($str) {
+    echo "\033[0;31mHyde !! {$str}\n\033[0m";
+}
 
-// create twig loader
-$loader = new Twig_Loader_Filesystem(__DIR__ . '/site');
-
-// create twig environment
-$twig = new Twig_Environment($loader);
+info('Welcome to Hyde Static Site Generator!');
+info('Begin by parsing parts');
 
 // grab variables to send to build
 $variable_helper = array();
 
+// set site file(helper variables basically)
+$site_file = __DIR__ . '/parts/site.yaml';
+
+// check for site.yaml
+if(is_file($site_file)) {
+    info("Found site.yaml, parsing into variables");
+
+    try {
+        // parse into site variables
+        $variable_helper['site'] = Yaml::parse(file_get_contents($site_file), true);
+    } catch(Exception $e) {
+            err("Error parsing file " . $site_file);
+            err($e->getMessage());
+            die("Exiting...\n");
+    }
+}
+
 // grab anything from parts directory
 $parts = glob(__DIR__ . '/parts/*', GLOB_ONLYDIR);
+
+info('Loading parts directories from ' . __DIR__ . '/parts');
 
 // loop through any parts
 foreach($parts as $part) {
     // set folder name
     $folder = basename($part);
+
+    info(" - Found folder '{$folder}', parsing now");
 
     // create empty array with name of directory
     $variable_helper[$folder] = array();
@@ -41,21 +65,82 @@ foreach($parts as $part) {
         // get the file
         $part_file = $part_files[$i];
 
+        // if we're in config ignore
+        if(basename($part_file) == 'config.yaml') {
+            $i++;
+            continue;
+        }
+
+        info(" -- Parsing " . basename($part_file) . " into variables");
+
         // try to parse the file
         try {
             $variable_helper[$folder][basename($part_file, '.yaml')] = Yaml::parse(file_get_contents($part_file), true);
         } catch(Exception $e) {
-            die('Uh oh there has been an error trying to parse some of your parts, message: ' . $e->getMessage());
+            err("Error parsing file " . $part_file);
+            err($e->getMessage());
+            die("Exiting...\n");
         }
 
         // next file
         $i++;
     } while($i < $c);
+
+    // config file
+    $config_file = $part . '/config.yaml';
+
+    // check for config
+    if(is_file($config_file)) {
+        info(" - Found config file {$folder}/config.yaml");
+
+        // parse config
+        $config = Yaml::parse(file_get_contents($config_file), true);
+
+        // check if they want to sort
+        if(array_key_exists('sort_by', $config) && array_key_exists('sort_order', $config)) {
+            info(" --- Sorting {$folder} by {$config['sort_by']} field in {$config['sort_order']} order");
+
+            // sort field
+            usort($variable_helper[$folder], function($a, $b) use($config) {
+                // set fields
+                $field_a = $a[$config['sort_by']];
+                $field_b = $b[$config['sort_by']];
+                info("Comparing {$field_a} to {$field_b}");
+
+                // check if we're comparing dates
+                if(stripos($config['sort_by'], 'date') !== false) return strtotime($field_a) < strtotime($field_b);
+            });
+        }
+
+        info(" -- Applying config to {$folder}");
+        print_r($variable_helper[$folder]);
+    }
 }
 
-// write file
-$fh = fopen(__DIR__ . '/build/' . 'index.html', 'w');
-fwrite($fh, $twig->render('index.html', $variable_helper));
+info("Finished parsing parts, moving onto site generation");
 
-// close file
-fclose($fh);
+info("Setting up Twig to parse templates");
+
+// create twig loader
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/site');
+
+// create twig environment
+$twig = new Twig_Environment($loader);
+
+// grab site pages
+$site_pages = glob(__DIR__ . '/site/*.html');
+
+info("Found " . count($site_pages) . " page(s) in " . __DIR__ . "/site");
+
+// loop through pages
+foreach($site_pages as $site_page) {
+    // grab page
+    $page = basename($site_page);
+
+    info(" - Rendering {$page}");
+
+    // write file
+    $fh = fopen(__DIR__ . '/build/' . $page, 'w');
+    fwrite($fh, $twig->render($page, $variable_helper));
+    fclose($fh);
+}
